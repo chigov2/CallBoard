d65639d6f19eab814bd2670d7c174678417ea199
chigov2 <chigov@gmail.com>
2021-09-02 20:19:33 +0300

    lesson4

--- app/src/main/java/chigovv/com/callboard/MainActivity.kt
+++ app/src/main/java/chigovv/com/callboard/MainActivity.kt
@@ -6,28 +6,35 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import chigovv.com.callboard.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
+
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+    //binding
+    //private var rootElement:ActivityMainBinding? = null
+    private lateinit var rootElement:ActivityMainBinding
+
     //добавляем слушатель нажатий для меню
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // initialization of var
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        //val view = rootElement.root
+        setContentView(rootElement.root)
         init()
     }
 
     private fun init(){
 
         //кнопка - необходимо создать два ресурса в string.xml и обратиться к ним яерез R.string
-        val toggle = ActionBarDrawerToggle(this,drawer_layout,toolbar,R.string.open,R.string.close)
+        val toggle = ActionBarDrawerToggle(this,rootElement.drawerLayout,rootElement.mainContent.toolbar,R.string.open,R.string.close)
 
         //указать что DraderLayout будет открываться через эту кнопку
-        drawer_layout.addDrawerListener(toggle)
+        rootElement.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         // связь navView and item selected listener
-        navView.setNavigationItemSelectedListener(this)
+        rootElement.navView.setNavigationItemSelectedListener(this)
 
 
     }
@@ -45,7 +52,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
             R.id.ac_sign_out -> {Toast.makeText(this,"Pressed ac_sign_out",Toast.LENGTH_SHORT).show()}
         }
         //чтобы меню закрывалось после нажатия
-        drawer_layout.closeDrawer(GravityCompat.START)
+        rootElement.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
