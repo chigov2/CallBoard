30e6c026b3636389e1b27b31733a1877945cbd16
chigov2 <chigov@gmail.com>
2021-09-08 09:14:34 +0300

    lesson2.3

--- app/src/main/java/chigovv/com/callboard/dialoghelper/DialogHelper.kt
+++ app/src/main/java/chigovv/com/callboard/dialoghelper/DialogHelper.kt
@@ -2,12 +2,18 @@
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.view.View
+import android.widget.Toast
+//import android.view.View.VISIBLE
+//import androidx.constraintlayout.solver.widgets.ConstraintWidget.VISIBLE
+//import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
 import chigovv.com.callboard.MainActivity
 import chigovv.com.callboard.R
 import chigovv.com.callboard.acoounthelper.AccountHelper
 import chigovv.com.callboard.databinding.SignDialogBinding
+//import com.google.firebase.database.core.view.View
 
-//создаем конструктор в круглых кавычках (их не было) но добавляем их и act:MainActivity
+ //создаем конструктор в круглых кавычках (их не было) но добавляем их и act:MainActivity
 class DialogHelper(act:MainActivity) {
     private val act = act
 
@@ -16,7 +22,7 @@ class DialogHelper(act:MainActivity) {
     //передаем индекс для выбора - вход/регистрация
     @SuppressLint("StringFormatInvalid")
 
-    public fun createSignDialog(index:Int){
+    fun createSignDialog(index:Int){
         //dialogBuilder - создаем спец класс
         val builder = AlertDialog.Builder(act)
         //теперь с помощью билдера создаем диалог - это обычный экран - при помощи binding
@@ -28,30 +34,61 @@ class DialogHelper(act:MainActivity) {
         //создали диалог, но он еще не рисуется на экране
         builder.setView(view)
 
-        // выбор окна
-        if (index == DialogConst.SIGN_UP_STATE)
-        {
-            rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
-            rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
-        }
-        else if (index  == DialogConst.SIGN_IN_STATE){
-            rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_in)
-            rootDialogElement.btSignUpIn.text =  act.resources.getString(R.string.sign_in_action)
-        }
+        setDialogState(index,rootDialogElement)
+
         val dialog = builder.create()
+
         rootDialogElement.btSignUpIn.setOnClickListener{
-            dialog.dismiss()
-            if (index == DialogConst.SIGN_UP_STATE){
-                accHelper.signUpWithEmail(rootDialogElement.etSignEmail.text.toString(),
-                    rootDialogElement.etSignPassword.text.toString())
-            }
-            else if ((index == DialogConst.SIGN_IN_STATE)){
-                accHelper.signInWithEmail(rootDialogElement.etSignEmail.text.toString(),
-                    rootDialogElement.etSignPassword.text.toString())
-            }
+            setOnClickSignUpIn(index,rootDialogElement,dialog)
         }
 
+        rootDialogElement.btForgetPass.setOnClickListener{
+            setOnClickResetPassword(rootDialogElement,dialog)
+        }
         //рисуем на экране
         dialog.show()
     }
-}
+
+     private fun setDialogState(index: Int, rootDialogElement: SignDialogBinding) {
+         // выбор окна
+         if (index == DialogConst.SIGN_UP_STATE)
+         {
+             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
+             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
+         }
+         else if (index  == DialogConst.SIGN_IN_STATE){
+             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_in)
+             rootDialogElement.btSignUpIn.text =  act.resources.getString(R.string.sign_in_action)
+             rootDialogElement.btForgetPass.visibility =android.view.View.VISIBLE
+         }
+     }
+
+     private fun setOnClickSignUpIn(index: Int, rootDialogElement: SignDialogBinding, dialog: AlertDialog?) {
+         dialog?.dismiss()
+         if (index == DialogConst.SIGN_UP_STATE){
+             accHelper.signUpWithEmail(rootDialogElement.etSignEmail.text.toString(),
+                 rootDialogElement.etSignPassword.text.toString())
+         }
+         else if ((index == DialogConst.SIGN_IN_STATE)){
+             accHelper.signInWithEmail(rootDialogElement.etSignEmail.text.toString(),
+                 rootDialogElement.etSignPassword.text.toString())
+         }
+     }
+
+     private fun setOnClickResetPassword(rootDialogElement: SignDialogBinding, dialog: AlertDialog?) {
+            if (rootDialogElement.etSignEmail.text.isNotEmpty()){
+                act.mAuth.sendPasswordResetEmail(rootDialogElement.etSignEmail.text.toString()).addOnCompleteListener { task ->
+                    //addOnCompleteListener выдет выдавать была ли успешная передача email
+                    if (task.isSuccessful){
+                        Toast.makeText(act,R.string.email_reset_pass_was_sent,Toast.LENGTH_LONG).show()
+                    }
+                }
+                dialog?.dismiss()
+            }
+
+         //если поле пароля пусто
+         else{
+             rootDialogElement.tvDialogMaessage.visibility = View.VISIBLE
+         }
+     }
+ }
