c6fe806e91b776d73ad20a2990a60adfbee98c68
chigov2 <chigov@gmail.com>
2021-09-16 20:25:32 +0300

    Lesson3.4

---
+++ app/src/main/java/chigovv/com/callboard/utils/imagePicker.kt
@@ -0,0 +1,27 @@
+package chigovv.com.callboard.utils
+
+import android.net.Uri
+import io.ak1.pix.models.Flash
+import io.ak1.pix.models.Mode
+import io.ak1.pix.models.Options
+import io.ak1.pix.models.Ratio
+
+//в этом классе будем получать картинки
+object imagePicker {
+
+    fun getImages(){
+        val options = Options().apply{
+            ratio = Ratio.RATIO_AUTO                                    //Image/video capture ratio
+            count = 3                                                   //Number of images to restrict selection count
+            spanCount = 4                                               //Number for columns in grid
+            path = "Pix/Camera"                                         //Custom Path For media Storage
+            isFrontFacing = false                                       //Front Facing camera on start
+            videoDurationLimitInSeconds = 10                            //Duration for video recording
+            mode = Mode.Picture                                             //Option to select only pictures or videos or both
+            flash = Flash.Auto                                          //Option to select flash type
+            preSelectedUrls = ArrayList<Uri>()                          //Pre selected Image Urls
+        }
+
+
+    }
+}
